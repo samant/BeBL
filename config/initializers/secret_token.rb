ab3862c2721a6f44fca8944d4210d48e2a31d314
Amant Stephane <stephane.amant@belighted.com>
2012-05-15 22:41:38 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GemListing::Application.config.secret_token = '7f22a9c05117c00f786a906924c9441f357dbb81de49b69d34ef2bb8b3954e40185d88ee935793acc034525fe4bfe721244b0892b4d9aeef9187ed1c5c401593'
